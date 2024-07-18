918a43be3e8ba17344b0bd1284ae89bc104cdd11
SenaTasci <tascisena99@gmail.com>
2024-07-18 23:38:11 +0300

    new download

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+﻿
+a = float(input("birinci sayıyı giriniz: "))
+b = float(input("ikinci sayıyı giriniz: "))
+
+print(a + b)
+print(a * b)
+print(a // b)
+print(a - b)
+
+print("\nThat's all, folks!")
---
+++ Time_Calculator.py
@@ -0,0 +1,10 @@
+﻿hour = int(input("Starting time (hours): "))
+mins = int(input("Starting time (minutes): "))
+dura = int(input("Event duration (minutes): "))
+
+a = ( mins + dura ) % 60 
+b = ( mins + dura ) // 60
+
+c = hour + b 
+
+print (c, ":", a )
